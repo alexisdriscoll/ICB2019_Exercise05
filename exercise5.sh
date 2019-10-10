a4b2ed21a2eee20ffef5456801543e10802bd5e5
Elizabeth Brown <ebrown20@nd.edu>
2019-10-09 22:37:19 -0400

    Hi

--- exercise5.sh
+++ exercise5.sh
@@ -1,4 +1,9 @@
-for line in wages.csv
-do
- cat $line | cut -d , -f 1,2 | tr ',' ' ' | sort -k1 sort -k2,2n > exercise5.txt
-done
+cat wages.csv | sed 1d | cut -d , -f 1,2 | tr ',' ' ' | sort -k1,1 -k2n  > exercise5.txt
+echo "Highest earner (gender yearsExperience wage): "
+cat wages.csv | sed 1d | cut -d , -f 1,2,4 | tr ',' ' ' | sort -k3n | tail -n 1
+echo "Lowest earner (gender yearsExperience wage): "
+cat wages.csv | sed 1d | cut -d , -f 1,2,4 | tr ',' ' ' | sort -k3n | head -n 1
+echo "Number of females in the top 10 earners: "
+cat wages.csv | sed 1d | cut -d , -f 1,2,4 | tr ',' ' ' | sort -k3n | tail -n 10 | grep female | wc -l
+
+
